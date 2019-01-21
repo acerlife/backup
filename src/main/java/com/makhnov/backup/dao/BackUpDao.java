727017cf9b9be2471a70e0ad769da6073858a506
acerlife <37626105+acerlife@users.noreply.github.com>
2019-01-21 12:03:51 +0200

    Delete BackUpDao.java

--- src/main/java/com/makhnov/backup/dao/BackUpDao.java
+++
@@ -1,7 +0,0 @@
-package com.makhnov.backup.dao;
-
-import com.makhnov.backup.model.BackUp;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BackUpDao extends JpaRepository<BackUp, Long> {
-}
